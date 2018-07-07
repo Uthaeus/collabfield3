058de47ef08003969efde175213cc0972584e68c
Uthaeus <romanlavery@gmail.com>
2018-07-07 15:27:02 -0600

    Added features specs for posts infinite scroll

--- app/assets/javascripts/posts/infinite_scroll.js
+++ app/assets/javascripts/posts/infinite_scroll.js
@@ -1,4 +1,5 @@
 $(document).on('turbolinks:load', function() {
+  var isLoading = false;
   if ($('.infinite-scroll', this).size() > 0) {
     $(window).on('scroll', function() {
       var more_posts_url = $('.pagination a.next_page').attr('href');
